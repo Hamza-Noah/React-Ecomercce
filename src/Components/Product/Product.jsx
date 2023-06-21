29e03364efb20f98d903a641348faf91c6351483
Hamza-Noah <hamza.nouh97@gmail.com>
2023-06-21 13:11:45 +0300

    added react router

---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Product(props) {
+  const { product } = props;
+
+  return (
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={product.image} className="card-img-top" alt={product.title} />
+        <div className="card-body">
+          <h5 className="card-title">{product.title}</h5>
+          <p className="card-text">{product.description}</p>
+          <a href="#" className="btn btn-primary">
+            Details
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/Components/ProductsList/ProductsList.jsx
+++ src/Components/ProductsList/ProductsList.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 
 export default function ProductsList() {
+  const api_url = "https://fakestoreapi.com/products";
+  const [products, setproducts] = useState([]);
+
+  useEffect(() => {
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => setproducts(data));
+  }, []);
+
   return (
-  <>
-    <h2 className='text-center'>
-        Our Procuts
-    </h2>
-  </>
-    )
+    <>
+      <h2 className="text-center p-3">Our Procuts</h2>
+      <div className="container">
+        <div className="row">
+          {products.map((product, index) => {
+            return (
+              <div className="col-3" key={++index}>
+                <Product product={product} key={++index}/>;
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
