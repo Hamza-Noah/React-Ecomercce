6a330a26384b66649fbe7624b2248b0f165c3411
Hamza-Noah <hamza.nouh97@gmail.com>
2023-07-04 13:39:20 +0300

    fixed the fetching in productDetails

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -6,23 +6,15 @@ import { useParams } from "react-router-dom";
 
 export default function ProductDetails() {
   const [product, setProduct] = useState({});
-  const id = useParams();
-
-  console.log(id);
+  const { id } = useParams();
 
   const API_URL = "https://fakestoreapi.com/products";
 
   useEffect(() => {
     fetch(`${API_URL}/${id}`)
-    .then(res=>res.json())
-    .then(json=>setProduct(json))
-  }, [])
-  
+      .then((res) => res.json())
+      .then((json) => setProduct(json));
+  }, []);
 
-
-  console.log(product);
-
-  return <>{
-    // <Product product={product} showButton={false} />
-    }</>;
+  return <>{<Product product={product} showButton={false} />}</>;
 }
