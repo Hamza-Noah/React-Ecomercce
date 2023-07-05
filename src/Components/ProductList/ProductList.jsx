e54d03440adb354eaedcc19097c7c0c421e398d0
Hamza-Noah <hamza.nouh97@gmail.com>
2023-07-05 14:07:46 +0300

    added category selction

--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -16,12 +16,18 @@ export default function ProductList() {
       .then((json) => setProducts(json));
   }, []);
 
+  function changeProductsCategory(category){
+    fetch(`https://fakestoreapi.com/products/category/${category}`)
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+  }
+
   return (
     <>
       <h1 className="text-center my-4">Our Produts</h1>
       {categories.map((category) => {
         return (
-          <button className="btn btn-info me-2 my-3" key={category}>
+          <button className="btn btn-info me-2 my-3" onClick={_ => changeProductsCategory(category)} key={category}>
             {category}
           </button>
         );
