56ceac41375f518a799bd3567c77864987d25735
Hamza-Noah <hamza.nouh97@gmail.com>
2024-10-27 12:40:34 +0300

    added the final fix to the project

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -12,8 +12,8 @@ export default function Product(props) {
         <div className="card h-100">
           <img src={product.image} className="card-img-top" alt="..." />
           <div className="card-body d-flex flex-column justify-content-between">
-            <h5 className="card-title">{product.title}</h5>
-            <p className="card-text">{product.description}</p>
+            <div>      <h5 className="card-title">{product.title}</h5>
+              <p className="card-text">{product.description}</p></div>
             <div className="d-flex justify-content-between align-items-center mt-4 flex-wrap ">
               <p>Price {product.price}$</p>
               <RatingStars rating={product.rating.rate} />
